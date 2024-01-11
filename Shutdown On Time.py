a2d46c952159ef2a7ad0494b8d63e7444f933e64
addaddadda <2417261933@qq.com>
2024-01-11 23:02:11 +0800

    A New Trying!

--- Shutdown On Time.py
+++ Shutdown On Time.py
@@ -2,5 +2,6 @@ import subprocess
 import time
 
 if __name__ == '__main__':
-    if time.localtime().tm_hour == 13:
-        subprocess.run(["shutdown", "-p"])
+    while True:
+        if time.localtime().tm_hour == 13:
+            subprocess.run(["shutdown", "-s"])
