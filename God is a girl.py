13ce02e403f76f2186fd3f6efb8dea3198c56f27
addaddadda <2417261933@qq.com>
2024-06-12 18:15:21 +0800

    a new one

--- God is a girl.py
+++ God is a girl.py
@@ -1,19 +1,25 @@
 import subprocess
 import time
-import getpass
-import os
 
-USER_NAME: str = getpass.getuser()
-file_path = os.path.dirname(os.path.realpath(__file__))
-bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
-with open(bat_path + '\\' + "GodRight", "w+") as bat_file:
-    bat_file.write(r'start "" "%s"' % file_path)
+start_time = time.time()  # 记录程序开始时间
+try:
+    while True:
+        # 获取当前小时（24小时制）
+        current_hour = int(time.strftime('%H'))
 
-while 1:
-    if 22 <= int(time.strftime('%H')):  # %d:Hour
-        subprocess.run(["shutdown", "-s", "-t", '0'])
-        exit()
-    elif time.strftime('%a') not in ['Mon', 'Sun']:  # %a:Week name(short for 3 word)
-        subprocess.run(["shutdown", "-s", "-t", '0'])
-        exit()
-    time.sleep(10)
+        present_time = time.time() - start_time  # 记录当前时间
+
+        # 当前时间晚于等于晚上9点时执行
+        if current_hour >= 21 or present_time >= 3600:
+            # 立即强制关机
+            subprocess.run(["shutdown", "-s", "-f", "-t", '1'])
+            exit()  # 程序执行关机命令后退出
+
+        # 检查频率：每10秒一次
+        time.sleep(10)
+
+except Exception as e:
+    # 记录异常日志
+    with open('error_log.txt', 'a') as f:
+        f.write(f"An error occurred at {time.strftime('%Y-%m-%d %H:%M:%S')}: {str(e)}\n")
+        f.close()
